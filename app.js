789907bea2efdf9598bc4a997eccc99851308de3
AlexZviadadze <117842458+AlexZviadadze@users.noreply.github.com>
2023-01-26 18:35:32 +0400

    Done

---
+++ app.js
@@ -0,0 +1,80 @@
+//(---------1---------)
+// function getNumbers(num) {
+//   if (num % 2 === 0) {
+//     console.log("false");
+//   } else {
+//     console.log("true");
+//   }
+// }
+
+// getNumbers(2);
+// getNumbers(197);
+
+//(--------2-------)
+
+// function getCurrencySymbolFromCode(parametrs) {
+//   if (parametrs === "USD") {
+//     console.log("$");
+//     return "$";
+//   }
+//   if (parametrs === "EUR") {
+//     console.log("€");
+//     return "€";
+//   }
+//   if (parametrs === "GEL") {
+//     console.log("₾");
+//     return "₾";
+//   }
+//   console.log("Currency sumbol is'n  find!!!");
+// }
+
+// getCurrencySymbolFromCode("GEL");
+// getCurrencySymbolFromCode("USD");
+// getCurrencySymbolFromCode("EUR");
+// getCurrencySymbolFromCode("RUB");
+
+// (---------------3---------------)
+// function getName(name) {
+//   console.log(name.toLowerCase());
+// }
+// getName("MY NAME IS ALEX");
+// getName("MY NAME IS GIORGI");
+
+// function getName(name) {
+//   console.log(name.toUpperCase());
+// }
+
+// getName("my name is alex");
+
+// (-----------------4----------)
+
+// const numbers = [12, 20, 43, 33, 37, 90, 221, 134, 543];
+// const result = numbers.filter((number) => number % 2 === 0);
+// console.log(result);
+
+// (--------------5-----------)
+
+// let arr = [
+//   { name: "alex", age: 20 },
+//   { name: "giorgi", age: 40 },
+// ];
+
+// function findPerson(arr, name) {
+//   console.log(arr, "alex");
+// }
+
+// findPerson(arr[0], "alex");
+
+// (---------------6-----------------)
+// function calcExpense(income, expense) {
+//   const profit = income - expense;
+//   //   console.log(profit);
+//   return profit;
+// }
+
+// const Jan = calcExpense(500, 250);
+// const Feb = calcExpense(200, 500);
+// const Mar = calcExpense(1000, 330);
+
+// const sumOfProfit = Jan + Feb + Mar;
+// console.log(sumOfProfit);
